8db201ef2ffa8b4583c5c808626f005d3ea25997
dineshbabu thoota <dineshbabu.thoota@hotmail.com>
2021-11-13 19:49:56 +0530

    Sign In API Changes

--- src/main/java/com/weighbridge/weighbridge/controllers/AuthController.java
+++ src/main/java/com/weighbridge/weighbridge/controllers/AuthController.java
@@ -1,11 +1,12 @@
 package com.weighbridge.weighbridge.controllers;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
-import com.weighbridge.weighbridge.payload.request.LoginRequest;
-import com.weighbridge.weighbridge.payload.response.JwtResponse;
-import com.weighbridge.weighbridge.security.jwt.JwtUtils;
-import com.weighbridge.weighbridge.security.service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,17 +20,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//import com.bezkoder.spring.security.postgresql.models.ERole;
-//import com.bezkoder.spring.security.postgresql.models.Role;
-
-//import com.bezkoder.spring.security.postgresql.payload.request.LoginRequest;
-//import com.bezkoder.spring.security.postgresql.payload.request.SignupRequest;
-//import com.bezkoder.spring.security.postgresql.payload.response.JwtResponse;
-//import com.bezkoder.spring.security.postgresql.payload.response.MessageResponse;
-//import com.bezkoder.spring.security.postgresql.repository.RoleRepository;
-//import com.bezkoder.spring.security.postgresql.repository.UserRepository;
-//import com.bezkoder.spring.security.postgresql.security.jwt.JwtUtils;
-//import com.bezkoder.spring.security.postgresql.security.services.UserDetailsImpl;
+import com.weighbridge.weighbridge.models.ERole;
+import com.weighbridge.weighbridge.models.Role;
+import com.weighbridge.weighbridge.models.User;
+import com.weighbridge.weighbridge.payload.request.LoginRequest;
+//import com.weighbridge.weighbridge.payload.request.SignupRequest;
+import com.weighbridge.weighbridge.payload.response.JwtResponse;
+//import com.weighbridge.weighbridge.payload.response.MessageResponse;
+//import com.weighbridge.weighbridge.repository.RoleRepository;
+import com.weighbridge.weighbridge.repository.UserRepository;
+import com.weighbridge.weighbridge.security.jwt.JwtUtils;
+import com.weighbridge.weighbridge.security.service.UserDetailsImpl;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -38,11 +39,8 @@ public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
 
-//    @Autowired
-//    UserRepository userRepository;
-
-//    @Autowired
-//    RoleRepository roleRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @Autowired
     PasswordEncoder encoder;
@@ -60,68 +58,13 @@ public class AuthController {
         String jwt = jwtUtils.generateJwtToken(authentication);
 
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-//        List<String> roles = userDetails.getAuthorities().stream()
-//                .map(item -> item.getAuthority())
-//                .collect(Collectors.toList());
 
         return ResponseEntity.ok(new JwtResponse(jwt,
                 userDetails.getId(),
                 userDetails.getUsername(),
-                userDetails.getEmail()));
+                userDetails.getEmail()
+                ));
     }
-//
-//    @PostMapping("/signup")
-//    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-//        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-//            return ResponseEntity
-//                    .badRequest()
-//                    .body(new MessageResponse("Error: Username is already taken!"));
-//        }
-//
-//        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-//            return ResponseEntity
-//                    .badRequest()
-//                    .body(new MessageResponse("Error: Email is already in use!"));
-//        }
-//
-//        // Create new user's account
-//        User user = new User(signUpRequest.getUsername(),
-//                signUpRequest.getEmail(),
-//                encoder.encode(signUpRequest.getPassword()));
-//
-//        Set<String> strRoles = signUpRequest.getRole();
-//        Set<Role> roles = new HashSet<>();
-//
-//        if (strRoles == null) {
-//            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//            roles.add(userRole);
-//        } else {
-//            strRoles.forEach(role -> {
-//                switch (role) {
-//                    case "admin":
-//                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-//                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//                        roles.add(adminRole);
-//
-//                        break;
-//                    case "mod":
-//                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-//                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//                        roles.add(modRole);
-//
-//                        break;
-//                    default:
-//                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//                        roles.add(userRole);
-//                }
-//            });
-//        }
-//
-//        user.setRoles(roles);
-//        userRepository.save(user);
-//
-//        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
-//    }
+
+
 }
--- src/main/java/com/weighbridge/weighbridge/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/weighbridge/weighbridge/security/service/UserDetailsServiceImpl.java
@@ -1,21 +1,25 @@
 package com.weighbridge.weighbridge.security.service;
 
 import com.weighbridge.weighbridge.models.User;
+import com.weighbridge.weighbridge.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-//    @Autowired
-//    UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-    //    @Transactional
+    @Override
+    @Transactional
     public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = userRepository.findByUsername(username)
-//                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
-        User user = new User( "trueram33", "trueram@gmail.com", "unmai");
-        user.setId(123);
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+//        User user = new User( "trueram33", "trueram@gmail.com", "unmai");
+//        user.setId(123);
         return UserDetailsImpl.build(user);
     }
 
